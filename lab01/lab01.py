23e0bc00d85315b3047eb5b1fde9bdea0cf6d886
Liam-Tucker3 <69359087+Liam-Tucker3@users.noreply.github.com>
2021-01-26 14:58:56 -0600

    Update lab01.py

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -22,7 +22,12 @@ def captured_output():
 
 # implement this function
 def is_perfect(n):
-    pass
+  goal = n
+  fac_sum = 0
+  for i in range(n):
+    if i != 0 and n % i == 0:
+      fac_sum += i
+  return fac_sum == goal  
 
 # (3 points)
 def test1():
@@ -40,7 +45,11 @@ def test1():
 
 # implement this function
 def multiples_of_3_and_5(n):
-    pass
+    mult_sum = 0
+    for i in range(n):
+      if i % 3 == 0 or i % 5 == 0:
+        mult_sum += i
+    return mult_sum 
 
 # (3 points)
 def test2():
@@ -53,7 +62,20 @@ def test2():
 # EXERCISE 3
 #################################################################################
 def integer_right_triangles(p):
-    pass
+    if p % 2 == 1:
+      return 0
+    counter = 0
+    # Every pythagorean triple, except (8, 15, 17), is a multiple of (x,(x * x - 1) / 2, (x * x + 1)/2)
+    for x in range(p//4):
+      side_a = x
+      side_b = (x * x - 1) / 2
+      side_c = (x * x + 1) / 2
+      perim = side_a + side_b + side_c
+      if x >= 3 and x % 2 == 1 and p % perim == 0:
+        counter += 1
+    if p % (8 + 15 + 17) == 0:
+      counter += 1
+    return counter
 
 def test3():
     tc = unittest.TestCase()
@@ -67,7 +89,21 @@ def test3():
 
 # implement this function
 def gen_pattern(chars):
-    pass
+  length = len(chars)
+  # print(length)
+  back = chars[::-1]
+  num_lines = 2 * length - 1
+  line_length = 2 * num_lines - 1
+  pattern = []
+  for i in range(length):
+    substring = back[:i+1] + chars[length - i:]
+    substring = '.'.join(substring)
+    substring = substring.center(line_length, '.')
+    pattern.insert(i, substring)
+    if i + 1 != length:
+      pattern.insert(i, substring)
+  for i in pattern:
+    print(i)
 
 def test4():
     tc = unittest.TestCase()
